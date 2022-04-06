e65ff4ecf6c4219da1ce2ec4bb6d1f6614ece88d
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-04-05 22:15:40 -0400

    "SKKD"

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const API = " https://pure-cove-46727.herokuapp.com/api/emojis"
+
+fetch(API)
+.then((res) => res.json())
+.then((data) => console.log(data))
+
+function emajiEmojis (data) {
+
+    
+
+
+}
