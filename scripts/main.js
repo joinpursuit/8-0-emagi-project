1dff53cc29345aedfb8d0d9665782207d4a373b2
ziqian pan <ziqianpan1998@gmail.com>
2022-04-08 18:58:42 -0400

    added q2, found new problem in 1 fixing

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 const BASE_URL = "https://pure-cove-46727.herokuapp.com/api/emojis"
 
-const forms = document.querySelectorAll("form")
-const form1 = forms[0];
+const forms = document.querySelectorAll("form");
 const results = document.querySelectorAll(".result");
+const form1 = forms[0];
 const result1 = results[0];
 const input1 = document.querySelector("#encode-phrase");
+//for searchEmoji
+const form2 = forms[1];
+const result2 = results[1];
+const input2 = document.querySelector("#search-phrase");//input of search phrase
 
 
 form1.addEventListener("submit", (event) => {
@@ -16,19 +20,17 @@ form1.addEventListener("submit", (event) => {
 })
 
 
-
 function getEmagiData(input1Text){
     fetch(`${BASE_URL}`)
     .then((response) => response.json())
     .then((json) => {
-   /// console.log(json);
-
-    encodedPhrase(json,input1Text);
-    })
+   /// console.log(json);   
+   encodedPhrase(json,input1Text);
+      })
     .catch((err) => {
         console.log(err);
     })
-   
+    input1.value = ""
 }
 
 function encodedPhrase(json,input1Text){
@@ -38,7 +40,7 @@ function encodedPhrase(json,input1Text){
         for (const singleData in json){
             for (let i = 0; i < input1Text.length; i++){
 
-            if (inputChar[i] === json[singleData].letter){//so close...it does not return all - just one for each uni inout
+            if (inputChar[i] === json[singleData].letter){//problem... my searching/result comes out alphbelt.
                 output1 += `${json[singleData].symbol}`;
                 //console.log(output)
             }
@@ -48,6 +50,47 @@ function encodedPhrase(json,input1Text){
     result1P.textContent = output1;
 
     }
+
+
+
+    form2.addEventListener("submit", (event) => {
+        event.preventDefault()
+        // console.log(input1Text);
+        const input2Text = input2.value
+        getEmagiData2(input2Text);
+   })
+
+//
+function getEmagiData2(input2Text){
+    fetch(`${BASE_URL}`)
+    .then((response) => response.json())
+    .then((json) => {
+   
+   //runs the search.
+    searchEmoji(json,input2Text);
+
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+   input2.value = "";
+}
+
+//search and return the emoji back.
+    function searchEmoji (json,input2Text){
+        const result2P = result2.querySelector("p");
+        let output2 = ""; 
+        for (const singleData in json){
+            if (json[singleData].name.includes(input2Text)){
+                //console.log("apple");
+                output2 += `${json[singleData].symbol}`;
+            }
+
+        }
+        result2P.textContent = output2;
+    }
+
+
 
 
 
