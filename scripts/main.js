42ba2a03724a0f373b87422eccfa661e237eef8f
ziqian pan <ziqianpan1998@gmail.com>
2022-04-08 01:00:18 -0400

    close to finishing 1

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+const BASE_URL = "https://pure-cove-46727.herokuapp.com/api/emojis"
+
+const forms = document.querySelectorAll("form")
+const form1 = forms[0];
+const results = document.querySelectorAll(".result");
+const result1 = results[0];
+const input1 = document.querySelector("#encode-phrase");
+
+
+form1.addEventListener("submit", (event) => {
+    event.preventDefault()
+    // console.log(input1Text);
+    const input1Text = input1.value
+    getEmagiData(input1Text);
+
+})
+
+
+
+function getEmagiData(input1Text){
+    fetch(`${BASE_URL}`)
+    .then((response) => response.json())
+    .then((json) => {
+   /// console.log(json);
+
+    encodedPhrase(json,input1Text);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+   
+}
+
+function encodedPhrase(json,input1Text){
+   const inputChar = input1Text.split("");//[a,d,f]
+    const result1P = result1.querySelector("p");
+    let output1 = ""; 
+        for (const singleData in json){
+            if (inputChar.includes(json[singleData].letter)){//so close...it does not return all - just one for each uni inout
+                output1 += `${json[singleData].symbol}`;
+                //console.log(output)
+            }
+        }
+    result1P.textContent = output1;
+
+    }
+
+
+
+
+
+
+
